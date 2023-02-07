4241cbfbe5d0fca70f98704e85018f75002b0614
Uros37 <uros.s037@gmail.com>
2023-02-07 22:54:48 +0100

    export certain html element

--- index.js
+++ index.js
@@ -10,16 +10,22 @@ const puppeteer = require('puppeteer');
   // Set viewport width and height
   await page.setViewport({ width: 1280, height: 720 });
 
-  const website_url = 'https://www.bannerbear.com/blog/how-to-convert-html-into-pdf-with-node-js-and-puppeteer/';
+  const website_url = 'http://localhost:8030/assets/static/output_html.html';
 
+  const selector = 'section.page';
   // Open URL in current page
-  await page.goto(website_url, { waitUntil: 'networkidle0' });
+  //await page.goto(website_url, { waitUntil: 'networkidle0' });
+  await page.goto(website_url);
+  await page.waitForSelector(selector);
+  const element = await page.$(selector);
 
   // Capture screenshot
-  await page.screenshot({
+//   await page.screenshot({
+//     path: 'screenshot.jpg',
+//   });
+  await element.screenshot({
     path: 'screenshot.jpg',
   });
-
   // Close the browser instance
   await browser.close();
 })();
