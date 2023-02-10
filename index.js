bea2a022bffc81fc2f659f55f0ef2971df37dbb4
Uros37 <uros.s037@gmail.com>
2023-02-10 04:04:52 +0100

    video recorder added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const { PuppeteerScreenRecorder } = require('puppeteer-screen-recorder');
 
 (async () => {
   // Create a browser instance
@@ -26,6 +27,38 @@ const puppeteer = require('puppeteer');
   await element.screenshot({
     path: 'screenshot.jpg',
   });
+
+  const Config = {
+    followNewTab: true,
+    fps: 60,
+    frames: 60 * 5, // 5 seconds at 60 fps
+    ffmpeg_Path: null,
+    videoFrame: {
+      width: 1024,
+      height: 768,
+    },
+    videoCrf: 18,
+    videoCodec: 'libx264',
+    videoPreset: 'ultrafast',
+    videoBitrate: 1000,
+    autopad: {
+      color: 'black' | '#35A5FF',
+    },
+    aspectRatio: '4:3',
+    recordDurationLimit: 20000
+  };
+  const recorder = new PuppeteerScreenRecorder(page, Config);
+  const savePath = 'demo.mp4';
+  await recorder.start(savePath, {omitBackground: true});
+  const time_need_to_record = 2;
+  await sleep(time_need_to_record * 1000);
+  await recorder.stop();
   // Close the browser instance
   await browser.close();
+
+  function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
 })();
